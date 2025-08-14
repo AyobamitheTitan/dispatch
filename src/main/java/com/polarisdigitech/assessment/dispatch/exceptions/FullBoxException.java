8c81c6c63a4f1e4b4b0fb037d27420cd741322a8
Oluwatimilehin <ayobamijijnr@gmail.com>
2025-08-14 13:45:50 +0100

    define full box exception

---
+++ src/main/java/com/polarisdigitech/assessment/dispatch/exceptions/FullBoxException.java
@@ -0,0 +1,7 @@
+package com.polarisdigitech.assessment.dispatch.exceptions;
+
+public class FullBoxException extends RuntimeException{
+    public FullBoxException(String message){
+        super(message);
+    }
+}
